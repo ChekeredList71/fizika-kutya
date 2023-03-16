3904c63c418e5f9c8b53496e198559de52ee2b77
Peter Nasz <naszpeti@gmail.com>
2023-03-16 15:47:41 +0100

    kutya implementálva
    tesztelés szükséges

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+//TODO: gui
 public class Main {
 
     static Scanner scn = new Scanner(System.in);
@@ -9,44 +9,70 @@ public class Main {
 
         Person p1 = new Person(0);
         Person p2 = new Person(0);
+        Person dog = new Person(0);
         double distance = 0;
         boolean notDoneChoosing = true;
         while (notDoneChoosing) {
             try {
                 System.out.print("1. személy sebessége: ");
-                p1 = new Person(scn.nextDouble());
+                p1.speed = scn.nextDouble();
 
                 System.out.print("2. személy sebessége: ");
-                p2 = new Person(scn.nextDouble());
+                p2.speed = scn.nextDouble();
+
                 System.out.print("Személyek távolsága: ");
                 distance = scn.nextDouble();
+
+                System.out.print("Köztük futó kutya sebessége: ");
+                dog.speed = scn.nextDouble();
+
+                notDoneChoosing = false;
             } catch (InputMismatchException e) {
                 System.out.println("A beírt adat nem szám. Csak egész vagy tizedes számokat használhat.");
-
-            } finally {
-                notDoneChoosing = false;
+                scn.nextLine(); //elviszi az előző sor enterét (nélküle loop lesz)
             }
         }
 
         double stepTime;
         double previousDistance = 0;
+        double dogDistanceRanInACycle = 0;
+        double dogDistanceRanAll = 0;
+        double dogCycle = 0;
 
         while (distance > 0) {
             try {
                 System.out.print("Következő pillanatig eltelt idő: ");
                 stepTime = scn.nextDouble();
+
                 previousDistance = distance;
-                distance = distance - p1.speed * stepTime - p2.speed * stepTime;
+
+                distance -= p1.speed * stepTime + p2.speed * stepTime;
+                dogDistanceRanInACycle += dog.speed * stepTime;
+                /*Ahányszor a kutya túlfutna az egyik emberen, hozzáadódik a köreihez egy szám.
+                  Ami a túlmenet, az hozzáadódik a következő körben futotthoz.
+                  HA NEM FUT TÚL: minusu lesz a következő körben a távolsága.
+                  EZ GOND????? TODO: megkérdezni a ha nem fut túl esetet*/
+                if (dogDistanceRanInACycle >= distance) {
+                    dogDistanceRanAll += dogDistanceRanInACycle;
+                    dogDistanceRanInACycle -= distance;
+                    dogCycle += 1;
+                }
+
             } catch (InputMismatchException e) {
                 System.out.println("A beírt adat nem szám. Csak egész vagy tizedes számokat használhat.");
             }
             System.out.println("Két személy távolsága: " + distance);
+            System.out.println("A kutya ennyiszer fordult meg eddig: " + dogCycle);
+            System.out.println("A kutya egy körön belül megtett távja (körönként nullázódik): "
+                    +dogDistanceRanInACycle);
+            System.out.println("A kutya összesen ennyit futott: " + dogDistanceRanAll);
+            System.out.println();
         }
 
         if (distance < 0) {
-            System.out.println(String.format("A személyek túlmentek egymáson. Ha %s idő telt volna el "+
-                    "az előző időponthoz képest, akkor találkoztak volna.", String.valueOf((previousDistance/(p1.speed
-                    + p2.speed)))));
+            System.out.printf("A személyek túlmentek egymáson. Ha %f idő telt volna el "+
+                    "az előző időponthoz képest, akkor találkoztak volna.%n", previousDistance / (p1.speed
+                    + p2.speed));
         } else {
             System.out.println("Személyek találkoztak.");
         }
